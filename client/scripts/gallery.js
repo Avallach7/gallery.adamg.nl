d2fe6cf8539b607bb622f339a16b5b253e3e90ce
Avallach7 <github@avallach.ovh>
2020-06-14 22:20:10 +0200

    add title

--- client/scripts/gallery.js
+++ client/scripts/gallery.js
@@ -4,5 +4,15 @@
 	async init() {
 		this.index = await (await fetch("/server/data-index.json")).json();
 		console.log("index", this.index);
+		if (location.pathname == "/") {
+			document.querySelector(".top-nav .back").style.display = "none";
+		}
+		const titleElement = document.querySelector(".content > .title");
+		if (titleElement.textContent == "") {
+			const path = location.pathname.split("/").filter(s => s != "");
+			const title = path[path.length-1].replace(/(^\w|\s\w)/g, m => m.toUpperCase());
+			document.title = title;
+			titleElement.textContent = title;
+		}
 	}
 }).init()
