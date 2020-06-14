f510e09d3cdda2c92dd8d62b415bcd8c4a8789fd
Avallach7 <github@avallach.ovh>
2020-06-14 20:25:09 +0200

    improve data index

--- client/scripts/gallery.js
+++ client/scripts/gallery.js
@@ -2,7 +2,7 @@
 	index: null,
 
 	async init() {
-		this.index = await (await fetch("/server/index.json")).json();
+		this.index = await (await fetch("/server/data-index.json")).json();
 		console.log("index", this.index);
 	}
 }).init()
