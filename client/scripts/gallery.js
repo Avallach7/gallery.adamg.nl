97a721bddc25378aa430c18933b7f7240f4fa49e
Avallach7 <github@avallach.ovh>
2020-06-20 16:49:44 +0200

    improve template

--- client/scripts/gallery.js
+++ client/scripts/gallery.js
@@ -1,16 +1,11 @@
-window.gallery = {
+const gallery = {
 	index: null,
 
 	async init() {
-		this.index = await (await fetch("/server/data-index.json")).json();
-		console.log("index", this.index);
+		console.log("init");
 		const titleElement = document.querySelector(".content > .title");
 		if (location.pathname == "/") {
-			document.querySelector(".top-nav .back").style.display = "none"; 
-			// TODO: show sidebar button instead
-			const siteTitleElement = document.querySelector(".top-nav .site-title");
-			titleElement.textContent = siteTitleElement.textContent;
-			siteTitleElement.textContent = "";
+			document.querySelector(".top-nav").style.display = "none"; 
 		}
 		if (titleElement.textContent == "") {
 			const path = location.pathname.split("/").filter(s => s != "");
@@ -20,22 +15,11 @@ window.gallery = {
 			document.title = title;
 			titleElement.textContent = title;
 		}
-		const descendants = this.index.filter(p => p.startsWith(location.pathname));
-		const existingDescendants = new Set(document.querySelectorAll(".files a"));
-		const parent = document.querySelector(".files");
-		for (var descendant of descendants) {
-			if (! existingDescendants.has(descendant)) {
-				const a = document.createElement("a");
-				a.href = descendant;
-				const name = descendant
-						.replace(new RegExp("^.*[\\\/]"), '')
-						.replace(new RegExp("\\.[^.]+$"), "");
-				a.textContent = name;
-				parent.appendChild(a);
-			}
-		}
+
 
 	}
 };
 
-window.gallery.init();
+// gallery.init();
+document.addEventListener("turbolinks:load", gallery.init.bind(gallery));
+window.gallery = gallery;
